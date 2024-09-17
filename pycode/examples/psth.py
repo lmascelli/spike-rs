50a0f8132fbe563cf2537642c34e5d81c49b1761
lmascelli <lmascelli@gmail.com>
2024-09-17 16:38:12 +0200

    psth example

--- pycode/examples/psth.py
+++ pycode/examples/psth.py
@@ -1,46 +1,16 @@
+from pycode.examples.psth import psth
 from pycode.handlers.phaseh5 import PhaseH5
-from pycode.operations import get_digital_intervals, subsample_range
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 # SET THE PATH OF THE FILE CONTAINING THE DATA CONVERTED WITH MULTICHANNEL DATA MANAGER
 phase_file = "/home/leonardo/Documents/unige/data/18-07-2024/38927/raw/2024-07-18T15-36-4438927_100E_DIV70_Stim70_0002_E-00155.h5"
-
-# OPEN THE PYCODE_RS HANDLER FOR THE DATA
 phase = PhaseH5(phase_file)
-
-sampling_frequency = phase.sampling_frequency()
 bin_time_duration = 10e-3  # duration of a bin IN SECONDS
-bin_size = int(
-    sampling_frequency * bin_time_duration
-)  # this round the size of a bin to the lower integer
-psth_duration = 500e-3  # duration of the psth IN SECONDS
-n_bins = int(psth_duration / bin_time_duration)  # number of bin after the stimulus
+psth_duration = 1000e-3  # duration of the psth IN SECONDS
 
-channels = phase.labels()  # list of all the available channels
+psth_vals = psth(phase, bin_time_duration, psth_duration)
 
-# get the number of digital channels. if it's different from 1 an error has occurred
-# during the recording phase
-n_digital = phase.n_digitals()
-if n_digital != 1:
-    exit(f"ERROR: the stimulation phase has {n_digital} digital channels")
-
-res = [0] * n_bins  # variable to accumulate the psth
-
-# read the digital channel
-digital = phase.digital(0)
-# get the interval timestamps where the stimulation is active
-digital_intervals = get_digital_intervals(digital)
-
-for interval in digital_intervals:
-    for channel in channels:
-        res = np.add(
-            res,
-            subsample_range(
-                phase.peak_train(channel, None, None)[0], interval[0], bin_size, n_bins
-            ),
-        )
-
-plt.bar(np.linspace(0, n_bins, n_bins), res)
+plt.bar(np.linspace(0, len(psth_vals), len(psth_vals)), psth_vals)
 plt.show()
--- pycode/operations.py
+++ pycode/operations.py
@@ -1,5 +1,7 @@
 from typing import List, Optional, Tuple
 import pycode_rs as pc
+from pycode.handlers.phaseh5 import PhaseH5
+import numpy as np
 
 
 def compute_threshold(
@@ -31,10 +33,59 @@ def subsampled_post_stimulus_times(
         phase._phase, bin_size, int(n_bins_post_stim), digital_index
     )
 
+
 def get_digital_intervals(digital: List[int]) -> List[Tuple[int, int]]:
     return pc.get_digital_intervals(digital)
+
 
 def subsample_range(
     peaks: List[int], starting_sample: int, bin_size: int, n_bins: int
 ) -> List[int]:
     return pc.subsample_range(peaks, starting_sample, bin_size, n_bins)
+
+
+def psth(phase: PhaseH5, bin_time_duration: float, psth_duration: float) -> List[int]:
+    """
+    Compute the PSTH ociaoooooooo :):):)
+    and returns a list with the count of the spikes in each bin.
+
+    @Parameters
+    - phase: the Phase of interest
+    - bin_time_duration: the duration of the bin IN SECONDS
+    - psth_duration: the duration of the whole psth IN SECONDS
+    """
+    # OPEN THE PYCODE_RS HANDLER FOR THE DATA
+    sampling_frequency = phase.sampling_frequency()
+    bin_size = int(
+        sampling_frequency * bin_time_duration
+    )  # this round the size of a bin to the lower integer
+    n_bins = int(psth_duration / bin_time_duration)  # number of bin after the stimulus
+
+    channels = phase.labels()  # list of all the available channels
+
+    # get the number of digital channels. if it's different from 1 an error has occurred
+    # during the recording phase
+    n_digital = phase.n_digitals()
+    if n_digital != 1:
+        exit(f"ERROR: the stimulation phase has {n_digital} digital channels")
+
+    res = [0] * n_bins  # variable to accumulate the psth
+
+    # read the digital channel
+    digital = phase.digital(0)
+    # get the interval timestamps where the stimulation is active
+    digital_intervals = get_digital_intervals(digital)
+
+    for interval in digital_intervals:
+        for channel in channels:
+            res = np.add(
+                res,
+                subsample_range(
+                    phase.peak_train(channel, None, None)[0],
+                    interval[0],
+                    bin_size,
+                    n_bins,
+                ),
+            )
+
+    return res
